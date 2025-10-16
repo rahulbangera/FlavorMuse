d19483f1f6e4433ed6931eb56d2c9e2b13852b12
Rahul N Bangera <bn2345890@gmail.com>
2025-10-16 22:04:40 +0530

    try fixes

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      "/ayur": {
-        target: "https://ayur-analytics-6mthurpbxq-el.a.run.app",
-        changeOrigin: true,
-        secure: true,
-        rewrite: p => p.replace(/^\/ayur/, ""),
-      },
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	server: {
+		allowedHosts: ["flavormuse.onrender.com"],
+		host: true,
+		port: 5173,
+		proxy: {
+			"/ayur": {
+				target: "https://ayur-analytics-6mthurpbxq-el.a.run.app",
+				changeOrigin: true,
+				secure: true,
+				rewrite: (p) => p.replace(/^\/ayur/, ""),
+			},
+		},
+	},
 });
